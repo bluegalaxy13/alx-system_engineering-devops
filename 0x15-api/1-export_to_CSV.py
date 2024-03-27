766b70803f7c267c5ac0cff4c1168dc2626f7136
bluegalaxy13 <ricardosassofficial@gmail.com>
2024-03-27 12:27:11 +0200

    updated second task in API activity

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
 from datetime import datetime
 from fabric.api import local
-
+import os.path
 
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
@@ -14,9 +13,11 @@ def do_pack():
                                                          dt.hour,
                                                          dt.minute,
                                                          dt.second)
+    
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
+        
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
     return file
