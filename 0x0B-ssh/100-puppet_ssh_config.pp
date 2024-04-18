171a9af176bb5df84077a7c22e6859fc50fb7936
Ricardo Sass <ricardosassofficial@gmail.com>
2024-04-18 16:32:43 +0200

    updated SSH files

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+# Automating my Tasks using Puppet
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+content => "
+    # SSH client configuration
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
