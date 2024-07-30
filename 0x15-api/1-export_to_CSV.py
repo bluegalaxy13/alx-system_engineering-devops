ab437eb6de2ccbd6b87f7e15ccc3430b384bd502
R Sass <83635268+bluegalaxy13@users.noreply.github.com>
2024-07-30 10:10:51 +0000

    Added documentaion

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Returns Todo list for a given employee id
-and axports data in the CSV format"""
+"""
+Returns Todo list for a given employee id and exports data in the CSV format.
+"""
+
 import requests
 from sys import argv
 
-if __name__ == "__main__":
+def main():
+    """
+    Main function to fetch user information and their todos,
+    then export the data to a CSV file.
+    """
     url = "https://jsonplaceholder.typicode.com/"
     USER_ID = argv[1]
 
@@ -15,5 +21,10 @@ if __name__ == "__main__":
     with open(file, "w") as f:
         for item in todos:
             f.write(
-                f'"{USER_ID}","{user["username"]}","{item["completed"]}", "{item["title"]}"\n'
+                '"{}","{}","{}","{}"\n'.format(
+                    USER_ID, user["username"], item["completed"], item["title"]
+                )
             )
+
+if __name__ == "__main__":
+    main()
